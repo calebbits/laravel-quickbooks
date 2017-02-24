0874d2ae714a6f431642fbf7e8805743a5742802
calebbits <calebbits@gmail.com>
2017-02-24 14:00:59 +0800

    updating connection with customized changes

--- src/Services/Connection.php
+++ src/Services/Connection.php
@@ -3,15 +3,57 @@
 namespace Myleshyson\LaravelQuickBooks\Services;
 
 use Myleshyson\LaravelQuickBooks\Quickbooks;
+use App\QuickBooks as QB;
 
 class Connection extends Quickbooks
 {
     /**
      * Connects to QuickBooks
      */
-    public function start()
+    //public function start()
+    public function start($organization_id,$qb_consumer_key,$qb_consumer_secret,$oauth_url,$success_url)
     {
-        if ($this->IntuitAnywhere->handle($this->config['the_username'], $this->config['the_tenant'])) {
+        //*************************************************************************************************
+        $this->config = config('quickbooks');
+        if (!\QuickBooks_Utilities::initialized($this->config['dsn'])) {
+            \QuickBooks_Utilities::initialize($this->config['dsn']);
+        }
+
+        $this->IntuitAnywhere = new \QuickBooks_IPP_IntuitAnywhere($this->config['dsn'], $this->config['encryption_key'], $qb_consumer_key, $qb_consumer_secret, $oauth_url, $success_url);
+
+        // Set up the IPP instance
+        $IPP = new \QuickBooks_IPP($this->config['dsn']);
+        if ($this->IntuitAnywhere->check($this->config['the_username'], $this->config['the_tenant']) and
+            $this->IntuitAnywhere->test($this->config['the_username'], $this->config['the_tenant'])) {
+            // Get our OAuth credentials from the database
+            $creds = $this->IntuitAnywhere->load($this->config['the_username'], $this->config['the_tenant']);
+            // Tell the framework to load some data from the OAuth store
+            $IPP->authMode(
+                \QuickBooks_IPP::AUTHMODE_OAUTH,
+                $this->config['the_username'],
+                $creds);
+
+            if ($this->config['sandbox']) {
+                // Turn on sandbox mode/URLs
+                $IPP->sandbox(true);
+            }
+
+            $qbObj = QB::where("organization_id",'=',$organization_id)->first();
+            $credentials = $qbObj->getFillable();
+            foreach($credentials as $field){
+                $qbObj[$field] = isset($creds[$field])?$creds[$field]:$qbObj[$field];
+            }
+            $qbObj->save();
+
+            // This is our current realm
+            $this->realm = $creds['qb_realm'];
+
+            // Load the OAuth information from the database
+            $this->context = $IPP->context();
+        }
+        //***************************************************************************************
+        $qb_id = QB::where("organization_id",'=',$organization_id)->first()->id;
+        if ($this->IntuitAnywhere->handle($qb_id,$this->config['the_username'], $this->config['the_tenant'])) {
             ; // The user has been connected, and will be redirected to QBO_SUCCESS_URL automatically.
         } else {
             // If obj happens, something went wrong with the OAuth handshake
